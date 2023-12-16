372d37c804465ccf7aa329faaffcaf3d184670b5
league <jake@thekeycrew>
2023-12-15 16:58:07 -0800

    i wrote and understood code

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -5,29 +5,46 @@ if __name__ == '__main__':
     window.bgcolor('white')
 
     # This code makes a new Turtle. Pick a new name for the turtle
-    my_turtle = turtle.Turtle()
+    jeffery_bezos = turtle.Turtle()
 
     # Make your turtle's shape 'turtle', .shape('turtle')
-
+    jeffery_bezos.shape('turtle')
     # Set your turtle's speed using .speed(2)
-
+    jeffery_bezos.speed(2)
     # Set your turtle's color using .color('green') and .pencolor('blue')
-
+    jeffery_bezos.color('green')
+    jeffery_bezos.pencolor('blue')
     # Move your turtle forward using .forward(100)
+    jeffery_bezos.forward(100)
     # TEST    Did your turtle move forward?
 
     # Move your turtle left or right using .left(90) or .right(90)
+    jeffery_bezos.left(90)
 
     # Now put the forward and left/right code into a for loop to repeat 4 times.
+    for i in range(4):
+        jeffery_bezos.forward(100)
+        jeffery_bezos.left(90)
     # TEST    Did your turtle draw a square?
 
     # Move your turtle to a new place on the screen using .goto(x, y)
+    jeffery_bezos.goto(100,-100)
+    jeffery_bezos.right(90)
+    jeffery_bezos.forward(100)
+    jeffery_bezos.left(90)
+    jeffery_bezos.forward(200)
+
+    for i in range(4):
+        jeffery_bezos.right(90)
+        jeffery_bezos.forward(100)
     # x=0 and y=0 is the center of the screen
 
     # Have your turtle draw a circle using .circle(radius, steps=30)
+    jeffery_bezos.circle(radius = 100 ,steps=)
     # TEST    Did your turtle draw a circle?
 
     # Add color to your shape by adding .begin_fill() before drawing the circle
+
     # and .end_fill() below
 
     # Draw 3 more shapes with different fill colors!
