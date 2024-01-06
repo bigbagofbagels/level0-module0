c3142af63f661909720ef4882a3d05b3ea8e2670
league <matthewseul@sbsd.net>
2024-01-05 16:58:04 -0800

    i made a pentagon that was colored :

--- _02_code_flow/_a_turtle_spiral.py
+++ _02_code_flow/_a_turtle_spiral.py
@@ -18,17 +18,17 @@ if __name__ == '__main__':
     # This code sets our shape to a turtle
     jeffery_bezos.shape('turtle')
     # Set your turtle's speed (0=fastest, 1=slowest, 10=faster)
-    
+    jeffery_bezos.speed(1)
     # Set your turtle's color using .color('green')
-    
+    jeffery_bezos.color('green')
     # Use a loop to repeat a the code below 50 times
-        
+    for i in range(50):
         # Set the turtle color to a random color
-
+        jeffery_bezos.color(get_random_color())
         # Move the turtle (5*i) pixels. 'i' is the loop variable
-        
-        # Turn the turtle (360/7) degrees to the right
-         
+        jeffery_bezos.forward(5*i)
+        jeffery_bezos.right(360/7)
+        jeffery_bezos.width(i)
         # Change the turtle width to 'i' (the loop variable)
         
         # Check the pattern against the picture in the recipe. If it matches, you are done!
